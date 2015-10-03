7c9f629429fc85e29966f91d46fc59db5e99fd6c
Melvin0496 <melvingabriel65@gmail.com>
2015-10-03 11:21:31 -0400

    Agregando EnumNeumaticos

--- EjerciciosCapitulo9Y10/EnumNeumaticos.Designer.cs
+++ EjerciciosCapitulo9Y10/EnumNeumaticos.Designer.cs
@@ -28,19 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.TipocomboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NeumaticosrichTextBox = new System.Windows.Forms.RichTextBox();
+            this.Verbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // TipocomboBox
+            // 
+            this.TipocomboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TipocomboBox.FormattingEnabled = true;
+            this.TipocomboBox.Items.AddRange(new object[] {
+            "Diagonal",
+            "Verano",
+            "Invierno",
+            "All Season",
+            "Asimetricos",
+            "Tubuless",
+            "Perfil bajo",
+            "Verdes",
+            "Run Flat"});
+            this.TipocomboBox.Location = new System.Drawing.Point(103, 59);
+            this.TipocomboBox.Name = "TipocomboBox";
+            this.TipocomboBox.Size = new System.Drawing.Size(121, 21);
+            this.TipocomboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tipo Neumatico:";
+            // 
+            // NeumaticosrichTextBox
+            // 
+            this.NeumaticosrichTextBox.Location = new System.Drawing.Point(12, 111);
+            this.NeumaticosrichTextBox.Name = "NeumaticosrichTextBox";
+            this.NeumaticosrichTextBox.Size = new System.Drawing.Size(390, 124);
+            this.NeumaticosrichTextBox.TabIndex = 2;
+            this.NeumaticosrichTextBox.Text = "";
+            // 
+            // Verbutton
+            // 
+            this.Verbutton.Location = new System.Drawing.Point(240, 59);
+            this.Verbutton.Name = "Verbutton";
+            this.Verbutton.Size = new System.Drawing.Size(75, 21);
+            this.Verbutton.TabIndex = 3;
+            this.Verbutton.Text = "Ver";
+            this.Verbutton.UseVisualStyleBackColor = true;
+            this.Verbutton.Click += new System.EventHandler(this.Verbutton_Click);
             // 
             // EnumNeumaticos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 261);
+            this.ClientSize = new System.Drawing.Size(414, 284);
+            this.Controls.Add(this.Verbutton);
+            this.Controls.Add(this.NeumaticosrichTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TipocomboBox);
             this.Name = "EnumNeumaticos";
             this.Text = "Enum neumaticos";
+            this.Load += new System.EventHandler(this.EnumNeumaticos_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox TipocomboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox NeumaticosrichTextBox;
+        private System.Windows.Forms.Button Verbutton;
     }
 }
--- EjerciciosCapitulo9Y10/EnumNeumaticos.cs
+++ EjerciciosCapitulo9Y10/EnumNeumaticos.cs
@@ -16,5 +16,63 @@ namespace EjerciciosCapitulo9Y10
         {
             InitializeComponent();
         }
+        enum Neumatico {Diagonal,Verano,Invierno,AllSeason,Asimetricos,Tubuless,PerfilBajo,Verdes,Runflat}
+
+        private void EnumNeumaticos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Verbutton_Click(object sender, EventArgs e)
+        {
+            Neumatico N;
+
+            if (TipocomboBox.Text.Equals("Diagonal"))
+            {
+               N = Neumatico.Diagonal;
+               NeumaticosrichTextBox.Text = N.ToString() + ": " + "se componen de capas de tejido alternas y creuxadas colocadas diagonalmente en la carcasa formando un angulo que suele estar entre 40 y 45 grados.";
+            }
+            if (TipocomboBox.Text.Equals("Verano"))
+            {
+                N = Neumatico.Verano;
+                NeumaticosrichTextBox.Text = N.ToString() + ": " + "Pueden utilizarse en cualquier epoca del año, pero su diseño y sus caracteristicas constructivas estan optimizadas para mejor agarre.";
+            }
+            if (TipocomboBox.Text.Equals("Invierno"))
+            {
+                N = Neumatico.Invierno;
+                NeumaticosrichTextBox.Text = N.ToString() + ": " + "cuentan con compuestos especiales para mejorar el agarre a bajas temperaturas, y una banda de rodadura con unas laminillas que se agarran a la nieve.";
+            }
+            if(TipocomboBox.Text.Equals("All Season"))
+            {
+                N = Neumatico.AllSeason;
+                NeumaticosrichTextBox.Text = N.ToString() + ": " + " que no llegan a ofrecer la prestaciones de los neumáticos de invierno en carreteras nevadas o heladas, pero aportan más ranuras y mayor profundidad en el dibujo que un neumático de verano, para conseguir un buen compromiso para usuarios que conducen en zonas invernales donde la temperatura exterior no es muy baja y las temporadas de nieve son breves.";
+            }
+            if (TipocomboBox.Text.Equals("Asimetricos"))
+            {
+                N = Neumatico.Asimetricos;
+                NeumaticosrichTextBox.Text = N.ToString() + ": " + "tienen diferente dibujo en la parte interior y la parte exterior del neumático, por ejemplo una parte optimizada para drenar el agua, y otra para mejorar el agarre en seco.";
+            }
+            if (TipocomboBox.Text.Equals("Tubuless"))
+            {
+                N = Neumatico.Tubuless;
+                NeumaticosrichTextBox.Text = N.ToString() + ": " + "Se llaman neumáticos tubuless a los que no necesitan una cámara interior para encerrar el aire.";
+            }
+            if(TipocomboBox.Text.Equals("Perfil bajo"))
+            {
+                N = Neumatico.PerfilBajo;
+                NeumaticosrichTextBox.Text = N.ToString() + ": " + "El perfil de un neumático es la altura de su flanco en milímetros. En la nomenclatura de un neumático, cuando nos dan sus dimensiones, viene determinado por la segunda cifra, que no indica la altura en sí, sino la relación entre la anchura –la primera cifra– y la altura.";
+            }
+            if (TipocomboBox.Text.Equals("Verdes"))
+            {
+                N = Neumatico.Verdes;
+                NeumaticosrichTextBox.Text = N.ToString() + ": " + "Cada vez son más frecuentes en las gamas de los fabricantes de neumáticos los denominados neumáticos ecológicos, fabricados con compuestos especiales y con un diseño de su dibujo en la banda de rodadura pensado para mejorar la resistencia al rodamiento y disminuir así el consumo de combustible.";
+            }
+            if(TipocomboBox.Text.Equals("Run Flat"))
+            {
+                N = Neumatico.Runflat;
+                NeumaticosrichTextBox.Text = N.ToString() + "" + "Los neumáticos runflat son un tipo especial de neumático con refuerzos en los flancos y los hombros, lo que les permite rodar una cierta distancia –entre unos 50 y 100 km– y a cierta velocidad –normalmente hasta 80 km/h– sin aire cuando se produce un pinchazo.";
+            }
+           
+        }
     }
 }
